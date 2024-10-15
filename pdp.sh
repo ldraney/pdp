e408683effbf822b8674c2c5a922b531bb693f7a
ldraney <draneylucas@gmail.com>
2024-10-14 23:58:59 -0600

    tmux clipboard working consistently

--- pdp.sh
+++ pdp.sh
@@ -1,33 +1,19 @@
 #!/bin/bash
 
 # Get the current directory from the first argument, or use the current directory if not provided
-CURRENT_DIR="${1:-$(pwd)}"
-
-# Build the SSH setup image
-docker build -t ssh-setup -f Dockerfile.ssh .
-
-# Build the main Docker image
-docker build -t pdp .
-
-# Create a Docker network for the containers to communicate
-docker network create pdp-network
+export CURRENT_DIR="${1:-$(pwd)}"
 
 # Display the SSH public key
 echo "Your SSH public key is:"
-docker run --rm ssh-setup cat /root/.ssh/id_rsa.pub
+docker compose run --rm ssh-setup
 
 echo "Please add this key to your GitHub account before proceeding."
 echo "Press any key to continue..."
 read -n 1 -s
 
-# Run the Docker container
-docker run -it --rm \
-  -p 8377:8377 \
-  -v "${HOME}/projects/pdp/bash/.bashrc:/root/.bashrc" \
-  -v "${HOME}/projects/pdp/bash/.bash_secrets:/root/.bash_secrets" \
-  -v "${CURRENT_DIR}:/root/workspace" \
-  -w "/root/workspace" \
-  pdp /bin/bash
+# Start the services
+docker compose up
 
-# Clean up the Clipper container and network when done
-# trap 'docker stop clipper-service; docker rm clipper-service; docker network rm pdp-network' EXIT
+# Clean up when done
+docker compose down --rmi all -v
+
